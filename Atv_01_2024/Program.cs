53264422e5ee81adf395b42a126f716268fc3751
Willian <willianf007@gmail.com>
2024-03-31 20:53:01 -0300

    first commit

---
+++ Atv_01_2024/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Preencha sua Ficha Cadastral ===");
+
+        // Solicita dados para o usuario 
+        Console.Write("Nome: ");                                             //nome 
+        string nome = Console.ReadLine();
+
+        Console.Write("Email: ");                                            //email 
+        string email = Console.ReadLine();
+
+        Console.Write("Data de Nascimento (DD/MM/AAAA): ");                   
+        DateTime dataNascimento = DateTime.Parse(Console.ReadLine());        //data de nascimento 
+
+        Console.Write("Sexo (M/F): ");                                       //sexo
+        char sexo = char.Parse(Console.ReadLine());
+
+        Console.Write("CEP: ");                                              //cep
+        int cep =  int.Parse(Console.ReadLine());
+
+        Console.Write("Rua: ");                                              //rua
+        string rua = Console.ReadLine();
+
+        Console.Write("Número: ");                                            //numero
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("Bairro: ");                                            //bairro
+        string bairro = Console.ReadLine();
+
+        Console.Write("Cidade: ");                                            //cidade
+        string cidade = Console.ReadLine();
+
+        Console.Write("UF: ");                                                //estado / uf
+        string uf = Console.ReadLine();
+
+        Console.Write("País: ");                                              //pais
+        string pais = Console.ReadLine();
+
+        // Exibe ficha cadastral
+        Console.WriteLine("\n=== Ficha Cadastral ===");
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Data de Nascimento: {dataNascimento.ToShortDateString()}");
+        Console.WriteLine($"Sexo: {sexo}");
+        Console.WriteLine($"CEP: {cep}");
+        Console.WriteLine($"Rua: {rua}");
+        Console.WriteLine($"Número: {numero}");
+        Console.WriteLine($"Bairro: {bairro}");
+        Console.WriteLine($"Cidade: {cidade}");
+        Console.WriteLine($"UF: {uf}");
+        Console.WriteLine($"País: {pais}");
+    }
+}
---
+++ Atv_03_2024/Atv_03_2024.cs
@@ -0,0 +1,43 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o operador aritmético desejado (+, -, *, /): ");
+        char operador = Console.ReadLine()[0]; // Obtém o primeiro caractere digitado pelo usuário
+
+        Console.WriteLine($"Tabuada do operador {operador}:");
+
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                int resultado = 0;
+
+                switch (operador)
+                {
+                    case '+':
+                        resultado = i + j;
+                        break;
+                    case '-':
+                        resultado = i - j;
+                        break;
+                    case '*':
+                        resultado = i * j;
+                        break;
+                    case '/':
+                        if (j != 0) // Evita divisão por zero
+                            resultado = i / j;
+                        break;
+                    default:
+                        Console.WriteLine("Operador inválido.");
+                        return;
+                }
+
+                Console.Write($"{i} {operador} {j} = {resultado,-3}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
